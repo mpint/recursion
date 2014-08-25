a2c2f8d4bd3c4f6a61cf1785892d53cec0b2ab82
Michael <mpinter09@gmail.com>
2014-08-25 04:12:05 -0700

    'stringifyJSON passes all tests'

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,64 +1,40 @@
-// this is what you would do if you liked things to be easy:
-// var stringifyJSON = JSON.stringify;
-
-// but you don't so you're going to write it from scratch:
-/* var stringifyJSON = function(obj) {
-
-console.log('start:',obj);
-  // your code goes here
-  try {
-	var objLen = Object.keys(obj).length;
-	
-	if (objLen === 0 && typeof obj === 'object' && obj.constructor === Array) {
-		var obj = '[]';
-		return (obj);
-	
-	}
-	
-	if (objLen && objLen > 0) {
-	
-		var keys = Object.keys(obj), i;
-			console.log('keys:',keys);
-		for ( i = 0; i < objLen; i++ ){
-			console.log(keys[i]);
-			stringifyJSON(keys[i]);
-		
-		}
-	}
-	
-  }
-  catch (e){
-	// catches all values not undefined and function
-		
-		if (typeof obj === 'number' || typeof obj === 'boolean' || typeof obj === 'undefined' || obj === null) {
-		console.log(typeof obj, obj, 'SUCCESS!');
-
-		return ('' + obj);
-		}
-		
-		if (typeof obj === 'string') {
-			console.log(typeof obj, obj, 'SUCCESS!');
-			return  '"' + obj + '"';
-		}
-
-	}
-   finally{}
-
-};
- */
 var stringifyJSON = function(obj) {
-	console.log(obj);
-	if (typeof obj === 'string') { return '"' + obj + '"';}
+console.log('CURRENT IN',obj);
+	if (_.isString(obj)) { return '"' + obj + '"';}
 	if ( obj && obj.constructor === Array) {
-		var len = obj.length, elemArray = [];
+		var len = obj.length,elemArray = [];
 		if (len === 0) { return '[]';}
 		
 		for (var i = 0; i < len; i++) {
+			if ( _.isFunction(obj[i]) || _.isUndefined(obj[i]) ) { 			
+				console.log('working arr');
+				return obj[i];
+			}
+			
 			elemArray.push( stringifyJSON( obj[i] ));
 			console.log('elemArray',elemArray);
 		}
 		console.log('finalArray',elemArray);
 		return ('[' + elemArray + ']');
 	}
+	if (obj && obj.constructor === Object) {
+		
+		if (_.isEmpty(obj)) { return '{}';}
+		
+		var keyArray = Object.keys(obj), keyLen = keyArray.length, totalArray = [];
+		
+		
+		for (var i = 0; i < keyLen; i++) {
+					
+			if ( _.isFunction( obj[keyArray[i]] ) || _.isUndefined( obj[keyArray[i]] ) ) {
+				console.log('working obj');
+				return '{}';
+			}
+			
+			totalArray.push( stringifyJSON( keyArray[i] ) + ':' + stringifyJSON( obj[keyArray[i]]) );
+		}
+		return ( '{' + totalArray.join() + '}' );
+	}
+	
 	return String(obj);
 };
